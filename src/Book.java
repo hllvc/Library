aa56492b765ef3b9c0cbd698d5bfdc0a3f0f2d33
Adis Halilovic <halilovic2105@gmail.com>
2019-04-28 01:31:14 +0200

    UPDATE: Created Book Class

---
+++ src/Book.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Book {
+
+	private String name;
+	private int number;
+	private boolean status;
+	private ArrayList<Integer> numbers = new ArrayList<>();
+	
+	protected Book(String name) {
+		this.name = name;
+		this.generateRandomNumber();
+		this.status = false;
+	}
+	
+	private void generateRandomNumber() {
+		boolean existing;
+		do {
+			existing = false;
+			this.number = (int)(Math.random() * 8999 + 1000);
+			for (int x: this.numbers)
+				if (x == this.number) {
+					existing = true;
+					break;
+				}
+		} while (existing);
+		numbers.add(this.number);
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	public boolean isStatus() {
+		return this.status;
+	}
+	
+	
+	
+}
